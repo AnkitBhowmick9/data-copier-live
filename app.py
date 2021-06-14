aeb49bb3626f912a8970f3262473e0fc19c50236
AnkitBhowmick9 <38423016+AnkitBhowmick9@users.noreply.github.com>
2021-06-14 10:10:07 +0530

    Added read access

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import sys
 from config import DB_DETAILS
-from util import get_tables
+from util import get_tables, load_db_details
+from read import read_table
 
 
 def main():
     """"Program atleast take 1 argument"""
     env = sys.argv[1]
-    db_details = DB_DETAILS[env]
+    # db_details = DB_DETAILS[env]
+    db_details = load_db_details(env)
     tables = get_tables('table_list')
-    for table in tables['table_name']:
-        print(table)
+    for table_name in tables['table_name']:
+        print(f'reading data for {table_name}')
+        data, column_name = read_table(db_details, tables)
+        print(f'reading data for {table_name}')
+    for rec in data:
+        print(rec)
+
+
 
 
 
